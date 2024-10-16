cfe0aba9682a95525cd8d145dcb37f11aa7c8cdd
Yaakov Carioca <douglasbpeixoto@gmail.com>
2024-10-16 11:47:50 -0300

    bugfix: babel runtime correction

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@babel/runtime'],
+      external: ['react-qr-scanner', '@babel/runtime'],
     },
   },
 });

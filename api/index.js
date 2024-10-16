b3dfbe45c5433e75a0147875c13cf61c63a84191
Yaakov Carioca <douglasbpeixoto@gmail.com>
2024-10-16 17:47:37 -0300

    bugfix

--- api/index.js
+++ api/index.js
@@ -1,23 +1,30 @@
 const express = require('express');
-const app = express();
+const mysql = require('mysql2');
+const cors = require('cors');
+const QRCode = require('qrcode');
+require('dotenv').config();
+
 const cadastrarRoute = require('./routes/cadastrar');
 const validarRoute = require('./routes/validar');
 const qrcodeRoute = require('./routes/qrcode');
 
-app.use(express.json());
-app.use('/cadastrar', cadastrarRoute);
-app.use('/validar', validarRoute);
-app.use('/gerar-qrcode', qrcodeRoute);
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Middleware para CORS e parsing de JSON
 app.use(cors({
-  origin: 'https://controllween.360brave.com/',
+  origin: 'https://controllween.360brave.com', // Substitua pela URL do seu frontend
   methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+// Rotas
+app.use('/cadastrar', cadastrarRoute);
+app.use('/validar', validarRoute);
+app.use('/gerar-qrcode', qrcodeRoute);
+
+// Iniciar o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });

442d644996651744502431aea3dd52e2fc7d6bca
Yaakov Carioca <douglasbpeixoto@gmail.com>
2024-10-16 13:12:19 -0300

    bf

--- api/cors.php
+++ api/cors.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: https://controllween.360brave.com");
-header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);

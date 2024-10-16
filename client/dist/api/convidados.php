f2d9efb8b1f477170e46bee95c4091b4023f6a6d
Yaakov Carioca <douglasbpeixoto@gmail.com>
2024-10-16 12:40:41 -0300

    bugfix

--- client/dist/api/convidados.php
+++
@@ -1,9 +0,0 @@
-<?php
-include './db/config.php';
-include 'cors.php';
-
-$stmt = $pdo->query("SELECT * FROM convidados ORDER BY nome ASC");
-$convidados = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode($convidados);
-?>

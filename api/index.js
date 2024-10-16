ccfb8392e047380399919e38cfdb56c1b4c156c4
Yaakov Carioca <douglasbpeixoto@gmail.com>
2024-10-16 17:33:55 -0300

    bugfix: testando abordagem emergencial de build&run

--- api/index.js
+++ api/index.js
@@ -9,6 +9,14 @@ app.use('/cadastrar', cadastrarRoute);
 app.use('/validar', validarRoute);
 app.use('/gerar-qrcode', qrcodeRoute);
 
+// Middleware para CORS e parsing de JSON
+app.use(cors({
+  origin: 'https://controllween.360brave.com/',
+  methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
